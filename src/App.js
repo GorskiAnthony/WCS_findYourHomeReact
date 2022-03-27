924fbf8ab05fec110e5dd956d6237154a7df8b58
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-27 10:22:59 +0200

    two step, filter available houses

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateCard from "./components/CreateCard";
 import houseToRent from "./houseToRent";
 
 function App() {
+	const [houses, setHouses] = useState(houseToRent);
+
+	const handleClick = (e) => {
+		if (e.target.checked) {
+			setHouses(houses.filter((house) => house.available));
+		} else {
+			setHouses(houseToRent);
+		}
+	};
+
 	return (
 		<div>
 			<header>
@@ -27,14 +37,15 @@ function App() {
 							className="available-checkbox"
 							name="checkbox"
 							id=""
+							onClick={handleClick}
 						/>
 					</div>
 				</div>
 				<div>
 					<div className="cards">
-						{houseToRent.map((house, id) => (
-							<CreateCard key={id} data={house} />
-						))}
+						{houses.map((data, id) => {
+							return <CreateCard key={id} data={data} />;
+						})}
 					</div>
 				</div>
 			</div>
